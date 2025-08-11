280587cbb188561c60d38d190bc982309b4cf56a
harish2428 <harishrockz2428@gmail.com>
2025-08-11 11:48:42 +0530

    new

---
+++ solution.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class CustomerTransactionProcessor:
+    def load_customers(self, filepath: str) -> pd.DataFrame:
+        return 
+
+    def load_transactions(self, filepath: str) -> pd.DataFrame:
+       pass
+    def clean_transaction_data(self, df: pd.DataFrame) -> pd.DataFrame:
+       
+       pass
+
+    def merge_data(self, transactions: pd.DataFrame, customers: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    def calculate_total_by_membership(self, merged_df: pd.DataFrame) -> pd.DataFrame:
+        pass
